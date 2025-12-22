69948ba7151ce7e9c98f06e8daca89d6db0442db
suzzzal <kshatrisujal@gmail.com>
2025-12-22 22:59:46 +0530

    Update locale.bulk.js

--- core/modules/locale/js/locale.bulk.js
+++ core/modules/locale/js/locale.bulk.js
@@ -19,7 +19,8 @@
       once('autodetect-lang', '#locale-translate-import-form', context);
       if (form.length) {
         const $form = $(form);
-        const $langcode = $form.find('.langcode-input');
+       const $langcodeSelect = ...
+       const $fileInput = ...
         $form.find('.file-import-input').on('change', function () {
           // If the filename is fully the language code or the filename
           // ends with a language code, pre-select that one.
